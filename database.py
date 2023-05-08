5abb25159190508dc4422a7dfd57a44cfc065ea6
med achraf ben fredj <57291591+ach02raf@users.noreply.github.com>
2023-05-08 14:05:11 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:achraf0000@database-achraf-2.cnfqgbotuw0t.us-east-1.rds.amazonaws.com/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:achraf0000@database-taf2.cnfqgbotuw0t.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
